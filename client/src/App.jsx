e216afa7864d99b30e3d5ceaddd8fbfd4a00fcc1
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-12 22:57:56 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,7 @@
+import Canvas from "./cancas";
+import Customizer from "./pages/customizer";
+import Home from "./pages/Home";
+
 function App() {
 	return <div className="head-text">Threejs</div>;
 }
