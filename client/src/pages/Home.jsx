976b771439059eb40413ec041fb7e83e6473aca9
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-13 20:02:51 +0300

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -10,7 +10,18 @@ import {
 } from "../config/motion";
 
 const Home = () => {
-	return <div>Home</div>;
+	const snap = useSnapshot(state);
+
+	return (
+		<AnimatePresence>
+			{snap.intro && (
+				<motion.div
+					className="home"
+					{...slideAnimation("left")}
+				></motion.div>
+			)}
+		</AnimatePresence>
+	);
 };
 
 export default Home;
