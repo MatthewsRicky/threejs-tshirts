f5a230a0295e9af3231b7badfeac217ca836a0a8
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-13 19:59:44 +0300

    Update index.js

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,4 +3,8 @@ import { proxy } from "valtio";
 const state = proxy((
   intro: true,
   color: '#EEFBD48',
+  isLogoTexturee: true,
+  isFullTexturee: true,
+  logoDecal: "./threejs.png",
+  fullDecal: "./threejs.png",
 ))
