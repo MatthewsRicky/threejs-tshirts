aff2a8ead8f436b6c5ca4b7fc0c26f9ffe0e8e9a
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-14 14:42:54 +0300

    Update CustomButton.jsx

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 
 const CustomButton = ({ type, title, handleClick, customStyles }) => {
+	const generateStyle = (type) => {
+		if (type === "filled") {
+			return {
+				backgroundColor: "#000",
+				color: "#fff",
+			};
+		}
+	};
+
 	return (
-		<button className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}>
+		<button
+			className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+			style={generateStyle(type)}
+		>
 			{title}
 		</button>
 	);
