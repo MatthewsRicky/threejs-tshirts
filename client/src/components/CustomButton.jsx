2698179a958cfe7da55f34db4eabad3e6cb0da3d
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-14 14:38:27 +0300

    Update CustomButton.jsx

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const CustomButton = ({ title }) => {
-	return <button className={`px-2`}>{title}</button>;
+const CustomButton = ({ type, title, handleClick, customStyles }) => {
+	return (
+		<button className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}>
+			{title}
+		</button>
+	);
 };
 
 export default CustomButton;
