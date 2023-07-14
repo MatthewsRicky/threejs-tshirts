1c0ae53bd2b7435a01c43341844d28710000ad14
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-14 10:58:33 +0300

    Update Customizer.jsx

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,3 +1,9 @@
+import { useState, useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useSnapshot } from "valtio";
+
+import config from "../config/config";
+
 const Customizer = () => {
 	return <div>Customizer</div>;
 };
