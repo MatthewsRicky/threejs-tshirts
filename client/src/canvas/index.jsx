f5554bb2d5f4fc564ce1f05e535ea2bc7ed63b30
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-14 16:11:56 +0300

    Update index.jsx

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -3,12 +3,11 @@ import { Environment, Center } from "@react-three/drei";
 import Shirt from "./Shirt";
 import Backdrop from "./Backdrop";
 import CameraRig from "./CameraRig";
-import { AmbientLight } from "three";
 
 const CanvasModel = () => {
 	return (
 		<Canvas>
-			<AmbientLight intensity={0.5} />
+			<ambientLight intensity={0.5} />
 			<Environment preset="city" />
 			{/*<CameraRig>}
 				{/*<Backdrop />*/}
