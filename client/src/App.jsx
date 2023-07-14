8ef97707aecd548581095ecd229f9e56cb046be7
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-14 08:23:08 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,13 @@ import Customizer from "./pages/Customizer";
 import Home from "./pages/Home";
 
 function App() {
-	return <div className="head-text">Home</div>;
+	return (
+		<main className="app transition-all ease-in">
+			<Home />
+			<Canvas />
+			<Customizer />
+		</main>
+	);
 }
 
 export default App;
