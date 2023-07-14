502d16e735dae882ea6adfb7e7e9089da4cca086
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-14 15:18:53 +0300

    Update Customizer.jsx

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -51,6 +51,19 @@ const Customizer = () => {
 							customStyles="w-fit px-4 py-2.5 font-bold text-sm"
 						/>
 					</motion.div>
+
+					<motion.div
+						className="filtertabs-container"
+						{...slideAnimation("up")}
+					>
+						{FilterTabs.map((tab) => (
+							<Tab
+								key={tab.name}
+								tab={tab}
+								handleClick={() => {}}
+							/>
+						))}
+					</motion.div>
 				</>
 			)}
 		</AnimatePresence>
