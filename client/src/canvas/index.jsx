e8b5ef5d370ae0ccf2ec2370c8f1891d8f7d18bb
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-14 15:44:31 +0300

    Update index.jsx

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,7 +1,23 @@
 import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
+import Shirt from "./Shirt";
+import Backdrop from "./Backdrop";
+import CameraRig from "./CameraRig";
+import { AmbientLight } from "three";
 
 const CanvasModel = () => {
-	return <div>Canvas</div>;
+	return (
+		<Canvas>
+			<AmbientLight intensity={0.5} />
+			<Environment preset="city" />
+			{/*<CameraRig>}
+				{/*<Backdrop />*/}
+			<Center>
+				<Shirt />
+			</Center>
+			{/*</Canvas></CameraRig>*/}
+		</Canvas>
+	);
 };
 
 export default CanvasModel;
