5e9e048c8ed7058aeae7c5b20f2cf74dfd92f580
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-14 15:04:13 +0300

    Update Customizer.jsx

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -18,7 +18,19 @@ import {
 
 const Customizer = () => {
 	const snap = useSnapshot(state);
-	return <AnimatePresence>{!snap.intro && <>Customizer</>}</AnimatePresence>;
+	return (
+		<AnimatePresence>
+			{!snap.intro && (
+				<>
+					<motion.div
+						key="custom"
+						className="absolute top-0 left-0 z-10"
+						{...slideAnimation("left")}
+					></motion.div>
+				</>
+			)}
+		</AnimatePresence>
+	);
 };
 
 export default Customizer;
